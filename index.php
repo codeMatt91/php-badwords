910dc52ce0de0c7f42ddbeabbfcc67c3fd2ea406
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-24 15:37:38 +0100

    Added file .php + bootstrap

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 
 
 
+$title='La mia prima applicazione PHP';
 
 
 
@@ -20,9 +21,12 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
    <title>Document</title>
 </head>
 <body>
-   
+   <div class="d-flex justify-content-center">
+   <h1><?php echo $title?></h1>
+   </div>
 </body>
-</html>
+</html>              
