b82eac3f693d0a3580bed5d620b25cdec5c4a5c4
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-04-22 16:27:04 +0200

    Fixed problem

--- index.php
+++ index.php
@@ -8,13 +8,11 @@ Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
 */
 
-
-
 $title = 'La mia prima applicazione PHP';
 $paragraph = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos minus deleniti enim possimus ipsam animi. Debitis neque quae animi fuga ipsa eligendi eveniet fugit, sit accusamus accusantium veniam asperiores! Adipisci? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos minus deleniti enim possimus ipsam animi. Debitis neque quae animi fuga ipsa eligendi eveniet fugit, sit accusamus accusantium veniam asperiores! Adipisci?';
 
-$name = $_GET['name'];
-$last_name = $_GET['lastname'];
+$name = $_GET['name'] ?? '';
+$last_name = $_GET['lastname'] ?? '';
 
 $censured = $_GET['censured'];
 
@@ -24,32 +22,37 @@ $censured = $_GET['censured'];
 <html lang="en">
 
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-   <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
 </head>
 
 <body>
-   <div class="d-flex flex-column text-center">
-      <h1><?php echo $title ?></h1>
-      <p><?php echo $paragraph ?></p>
-      <p>il mio titolo e lungo <?php echo strlen($paragraph) ?> lettere</p>
+    <div class="d-flex flex-column text-center">
+        <h1><?php echo $title; ?></h1>
+        <p><?php echo $paragraph; ?></p>
+        <p>il mio titolo e lungo <?php echo strlen($paragraph); ?> lettere</p>
 
-      <div>Questo lavoro e stato realizzato da <?php echo $name . $last_name ?></div>
+        <div>Questo lavoro e stato realizzato da <Strong><?php echo "$name $last_name"; ?></Strong></div>
 
-      <div><?php echo str_replace('ipsa', $censured, $paragraph) ?></div>
+        <div><?php echo str_replace('ipsa', $censured, $paragraph); ?></div>
 
-      <div class="my-5">
+        <div class="my-5">
 
-         <form action="index.php" method="get">
-            <label for="word">inserisci la parola con cui vuoi censurare</label>
-            <input id="word" type="text" name="censured">
-            <button type="submit">INVIA</button>
-         </form>
-      </div>
+            <form action="index.php" method="get">
+                <label for="name">inserisci il nome</label>
+                <input id="name" type="text" name="name">
+                <label for="lastname">inserisci il cognome</label>
+                <input id="lastname" type="text" name="lastname">
+                <label for="word">inserisci la parola con cui vuoi censurare</label>
+                <input id="word" type="text" name="censured">
+                <button type="submit">INVIA</button>
+            </form>
+        </div>
 
-   </div>
+    </div>
 </body>
 
-</html>              
+</html>
